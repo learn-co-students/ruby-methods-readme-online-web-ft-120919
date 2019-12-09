a7e58190886544691696e03ffeab40aa30a1bdb5
Steven Kumarsingh <stevenks17@gmail.com>
2019-12-09 13:47:22 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -2,14 +2,18 @@
 def greeting
   puts "Hello world"
 end  
+
+def phrase
+  
 phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+ puts phrase
+end 
